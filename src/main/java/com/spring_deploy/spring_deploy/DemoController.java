ffa88db21eb39fa926331a120d53acbeed5fa29e
AiwendilCiunio <ciunio@gmail.com>
2025-05-03 22:29:20 +0200

    first commit

---
+++ src/main/java/com/spring_deploy/spring_deploy/DemoController.java
@@ -0,0 +1,16 @@
+package com.spring_deploy.spring_deploy;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/")
+@RestController
+public class DemoController {
+
+    @GetMapping
+    public ResponseEntity<String> getRoot() {
+        return ResponseEntity.ok("{\"message\":\"Hello World\"}");
+    }
+}
